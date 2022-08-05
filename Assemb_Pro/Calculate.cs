0f9074f28f7ca554142ddcb0a799e6d0186e66a2
Akhil P Thomas <akhilthomas1498@gmail.com>
2022-08-05 11:37:13 +0530

    Add project files.

---
+++ Assemb_Pro/Calculate.cs
@@ -0,0 +1,14 @@
+﻿namespace Assemb_Pro
+{
+    public class Calculate
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
